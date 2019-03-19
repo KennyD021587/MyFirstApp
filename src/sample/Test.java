124dd6867df1f4c231eb79ab5f6aab925c1e86ee
Kenny D <KennyD@Viewlifts-MacBook-Pro-10.local>
2019-03-19 14:11:34 -0400

    changed file to for the last time for jenkins to work

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -1,23 +1,36 @@
-package sample;
-
-public class Test {
-
-	public static void main1(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello");
-	}
+//package sample;
+//
+//public class Test {
+//
+//	public static void main1(String[] args) {
+//		// TODO Auto-generated method stub
+//System.out.println("Hello");
+//	}
 
 //public static class MyFirstApp {
 //	
 //}
 	
-	public static void main(String[] args) {
-		System.out.print("I Rule");
-		System.out.print("The World");
-		}
-	}
-		
+//	public static void main(String[] args) {
+//		System.out.print("I Rule");
+//		System.out.print("The World");
+//		}
+//	
+//	}
+//		
 
+		// starting new to see if jenkins will work for me 
+
+package sample;
+
+public class Test {
+	
+	public static void main (String[] args) {
+		System.out.print("I Rule!");
+		System.out.print("The World");
 		
+	}
+
+}
 	
 		
